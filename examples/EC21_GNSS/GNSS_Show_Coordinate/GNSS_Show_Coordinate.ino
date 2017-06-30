dff1e34dc2d2c8d36954507b72feceebea2dbef6
lanselambor <757016515@qq.com>
2017-06-30 14:32:31 +0800

    examples/EC21_GNSS/GNSS_Show_Coordinate: add function getCordinate

--- examples/EC21_GNSS/GNSS_Show_Coordinate/GNSS_Show_Coordinate.ino
+++ examples/EC21_GNSS/GNSS_Show_Coordinate/GNSS_Show_Coordinate.ino
@@ -1,22 +1,33 @@
-#include "MC20_Common.h"
-#include "MC20_Arduino_Interface.h"
-#include "MC20_GNSS.h"
+#include "module_common.h"
+#include "Arduino_Interface.h"
+#include "gnss.h"
 
 
 GNSS gnss = GNSS();
 
 void setup() {
-  SerialUSB.begin(115200);
-  // while(!SerialUSB);
+  // Enable Module Power
+  pinMode(gnss.MODULE_PWR_PIN, OUTPUT);
+  digitalWrite(gnss.MODULE_PWR_PIN , HIGH);    
+  // Enable VCCB
+  pinMode(gnss.ENABLE_VCCB_PIN, OUTPUT);
+  digitalWrite(gnss.ENABLE_VCCB_PIN, HIGH);
 
+  // Module power on
   gnss.Power_On();
+  while(false == gnss.Check_If_Power_On()){
+    SerialUSB.println("Waitting for module to alvie...");
+    delay(1000);
+  } 
   SerialUSB.println("\n\rPower On!");
 
-  while(!gnss.open_GNSS(EPO_QUICK_MODE)){
-    delay(1000);
+  if(!(gnss.open_GNSS())){
+    SerialUSB.println("\n\rGNSS init failed!");
+    return;
   }
 
   SerialUSB.println("Open GNSS OK.");
+  delay(2000);
 }
 
 void loop() {
